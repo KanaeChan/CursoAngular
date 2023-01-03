252bf67136e4e209ebd6a7f4e5215ac8fc0cbc55
Servicio Social ccr <ssdesp-ccr@cofepris.gob.mx>
2023-01-03 09:57:39 -0600

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Tarjeta{
+  titulo:string;
+  subtitulo:string;
+  nro?:number; //El ? significa que este valor puede ser nulo o no
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Miprimerapp';
+  public ArregloTarjetas:Tarjeta[]=[]
+
+  ngOnInit(){
+
+    this.ArregloTarjetas = [
+      {titulo: 'Video 1', subtitulo: 'Subtitulo Video 1'},
+      {titulo: 'Video 2', subtitulo: 'Subtitulo Video 2'},
+      {titulo: 'Video 3', subtitulo: 'Subtitulo Video 3'},
+    ]
+  }
 }
